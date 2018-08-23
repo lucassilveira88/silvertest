38fa6992736491fdba533c9188e9fe0427dcef84
Silveira <Lucas_G_Silveira@Dell.com>
2018-08-23 17:41:15 -0300

    oi2

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var app = express();
+var http = require('http'),
+    fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
