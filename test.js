89cd39cbf656c0345b2ee9fea3cfcac1b991c84b
Silveira <Lucas_G_Silveira@Dell.com>
2018-08-23 17:28:55 -0300

    oi

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World");
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
