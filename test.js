5b33f35c531f8b588571fe19dc4884c5d14d19eb
unknown <lucas_g_silveira@outlook.com>
2018-08-24 11:25:53 -0300

    procfile

--- test.js
+++ test.js
@@ -1,14 +1,7 @@
-var http = require('http'),
-    fs = require('fs');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(3000);
-});
+express()  
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
